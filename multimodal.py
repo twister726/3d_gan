cd89ba0640294d01bf5097d81dc66f4171733a22
Anubhab Sen <anubhabsen@gmail.com>
2017-10-27 00:14:45 +0530

    Gan file

---
+++ multimodal.py
@@ -0,0 +1,50 @@
+from keras.models import Sequential
+from keras.layers import Dense, Dropout
+from keras.layers import Embedding
+from keras.layers import LSTM
+
+def lstm_model():
+	model = Sequential()
+	model.add(Embedding(20000, 128))
+	model.add(LSTM(128))
+	model.summary()
+	return model
+
+def generator_model():
+    model = Sequential()
+    model.add(Dense(input_dim=100, output_dim=1024))
+    model.add(Activation('tanh'))
+    model.add(Dense(128*7*7))
+    model.add(BatchNormalization())
+    model.add(Activation('tanh'))
+    model.add(Reshape((7, 7, 128), input_shape=(128*7*7,)))
+    model.add(UpSampling2D(size=(2, 2)))
+    model.add(Conv2D(64, (5, 5), padding='same'))
+    model.add(Activation('tanh'))
+    model.add(UpSampling2D(size=(2, 2)))
+    model.add(Conv2D(1, (5, 5), padding='same'))
+    model.add(Activation('tanh'))
+    return model
+
+def discriminator_model():
+    model = Sequential()
+    model.add(Conv2D(64, (5, 5), padding='same', input_shape=(28, 28, 1)))
+    model.add(Activation('tanh'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Conv2D(128, (5, 5)))
+    model.add(Activation('tanh'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Flatten())
+    model.add(Dense(1024))
+    model.add(Activation('tanh'))
+    model.add(Dense(1))
+    model.add(Activation('sigmoid'))
+    return model
+
+def lstm_generator_containing_discriminator(l, g, d):
+    model = Sequential()
+    model.add(l)
+    model.add(g)
+    d.trainable = False
+    model.add(d)
+    return model
